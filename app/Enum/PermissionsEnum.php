2feb8dc7182c4b269fd27fa138eaad8124045986
sadek1999 <sadekali1530@gmail.com>
2024-11-26 16:55:20 +0600

    successfully seed roles

--- app/Enum/PermissionsEnum.php
+++ app/Enum/PermissionsEnum.php
@@ -2,7 +2,10 @@
 
 namespace App\Enum;
 
-enum PermissionsEnum
+enum PermissionsEnum:string
 {
-    //
+    case ManageUsers='manage_users';
+    case ManageComments='mange_comments';
+    case ManageFeatures='manage_features';
+    case Upvote_Downvote='upvote_downvote';
 }
--- app/Enum/RolesEnum.php
+++ app/Enum/RolesEnum.php
@@ -2,7 +2,28 @@
 
 namespace App\Enum;
 
-enum RolesEnum
+
+
+enum RolesEnum: string
 {
-    //
+    case Admin = 'admin';
+    case Commenter = 'commenter';
+    case User = 'user';
+
+    public static function labels(): array
+    {
+        return [
+            self::Admin->value => 'admin',
+            self::User->value => 'user',
+            self::Commenter->value => 'commenter'
+        ];
+    }
+    public function label()
+    {
+        return match ($this) {
+            self::Admin => 'admin',
+            self::User => 'user',
+            self::Commenter => 'commenter'
+        };
+    }
 }
